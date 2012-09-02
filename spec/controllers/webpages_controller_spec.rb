7a0c4f9b62be8840a2d070b1829be27d3df5229b
Tim Tyrrell <email@timtyrrell.net>
2012-09-02 16:37:44 -0500

    Failing show slug spec

--- spec/controllers/webpages_controller_spec.rb
+++ spec/controllers/webpages_controller_spec.rb
@@ -21,16 +21,21 @@ describe WebpagesController do
     end
   end
 
+  describe "GET 'show'" do
+    it "should redirect to the url from a slug" do
+      webpage = Webpage.new(:url => "www.google.com", :slug => "placeholder")
+      Webpage.stub(:find_by_slug).with("placeholder").and_return(webpage)
+      get 'show', :slug => webpage.slug
+      should redirect_to(webpage.url)
+    end
+
+    it "should redirect to the entry page if the slug is not found"
+
+  end
+
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'show'" do
-    pending "returns http success" do
-      get 'show'
       response.should be_success
     end
   end
