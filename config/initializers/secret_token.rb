0408c38ec8a2f2b0528c535f18b12c8f2ae8e47a
Tim Tyrrell <email@timtyrrell.net>
2012-09-02 11:24:42 -0500

    Project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shortener::Application.config.secret_token = 'f2db8f07e211dec1e4fd55139baec3f3462685c3f093ad1787ff884feedaa36bfdda1a3a613e00a79f11bc925702fa0e318d8b1ba72295aec6ba8a5a99adc92f'
